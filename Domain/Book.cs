811a22e9e51e64a4400dbc8b598d4df9414097d1
Baran Sarıtaş <75392592+Baran-Saritas@users.noreply.github.com>
2022-02-16 14:20:27 +0300

    Temel yapı ile base entıtıy ve ondan kalıtım alan aclasslar olusturuldu

---
+++ Domain/Book.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Book : BaseEntity
+    {
+        public string book_name { get; set; }
+        public bool alinma_bilgisi { get; set; }
+
+
+    }
+}
---
+++ Domain/Student.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Student :BaseEntity
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int number { get; set; }
+
+    }
+}
